bc59afef121b39e370b9a718e9971c93f6e14840
NDHunq <22520514@gm.uit.edu.vn>
2024-07-15 14:10:35 +0700

    .

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,41 +1,60 @@
-import userService from '../services/userService'
-let handleLogin=async(req,res)=>{
-    let mail=req.body.email;
-    let pass=req.body.password;
+import userService from "../services/userService";
+let handleLogin = async (req, res) => {
+  let mail = req.body.email;
+  let pass = req.body.password;
 
-    if(!mail||!pass)
-    {
-        return res.status(500).json({
-            errCode:1,
-            message:'Missing input parameter'
-        });
-    }
-
-    let userData=await user.handleUserLogin(mail,pass);
-    return res.status(200).json({
-        errCode:userData.errCode,
-        message:userData.errMessage,
-        user:userData.user?userData.user:{}
+  if (!mail || !pass) {
+    return res.status(500).json({
+      errCode: 1,
+      message: "Missing input parameter",
     });
-}
-let handleGetAllUsers=async(req,res)=>{
-    let id=req.query.id;
-    if(!id)
-    {
-        return res.status(200).json({
-            errCode:1,
-            message:'Missing input parameter'
-        });
-    }
-    let users=await userService.getAllUsers(id);
-    return res.status(200).json({
-        errCode:0,
-        message:'0',
-        users
-    });
-}
+  }
 
-module.exports={
-    handleLogin:handleLogin,
-    handleGetAllUsers:handleGetAllUsers
-}
+  let userData = await user.handleUserLogin(mail, pass);
+  return res.status(200).json({
+    errCode: userData.errCode,
+    message: userData.errMessage,
+    user: userData.user ? userData.user : {},
+  });
+};
+let handleGetAllUsers = async (req, res) => {
+  let id = req.query.id;
+  if (!id) {
+    return res.status(200).json({
+      errCode: 1,
+      message: "Missing input parameter",
+    });
+  }
+  let users = await userService.getAllUsers(id);
+  return res.status(200).json({
+    errCode: 0,
+    message: "0",
+    users,
+  });
+};
+let handleCreateNewUser = async (req, res) => {
+  let message = await userService.createNewUser(req.body);
+  return res.status(200).json(message);
+};
+let handleEditUser = async (req, res) => {
+  let data = req.body;
+  let message = await userService.updateUserData(data);
+  return res.status(200).json(message);
+};
+let handleDeleteUser = async (req, res) => {
+  if (!req.body.id)
+    return res.status(200).json({
+      errCode: 1,
+      message: "Missing input parameter",
+    });
+  let message = await userService.deleteUser(req.body.id);
+  return res.status(200).json(message);
+};
+
+module.exports = {
+  handleLogin: handleLogin,
+  handleGetAllUsers: handleGetAllUsers,
+  handleCreateNewUser: handleCreateNewUser,
+  handleEditUser: handleEditUser,
+  handleDeleteUser: handleDeleteUser,
+};
